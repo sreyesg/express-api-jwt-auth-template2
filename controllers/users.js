25c51d22b8fee85505d15b3a9f7265379007dc73
sreyesg <santiagoreyes.g@gmail.com>
2024-11-13 20:46:03 -0800

    Refactor /signup route to issue token inside route's execution context

--- controllers/users.js
+++ controllers/users.js
@@ -17,7 +17,12 @@ router.post('/signup', async(req, res) => {
             username: req.body.username,
             password: bcrypt.hashSync(req.body.password, SALT_LENGTH)
         })
-        res.status(201).json({ user })
+
+        const token = jwt.sign(
+            {username: user.username, _id: user._id},
+            process.env.JWT_SECRET
+        )
+        res.status(201).json({ user, token })
         
     } catch (error) {
         res.status(400).json({error: error.message})
