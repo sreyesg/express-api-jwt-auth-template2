8aa3cc1f6a3956078af0b6592887b8b8ca571d6a
sreyesg <santiagoreyes.g@gmail.com>
2024-11-13 19:22:55 -0800

    Define JWT token verification route

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -12,4 +12,16 @@ router.get('/sign-token', (req, res) => {
     res.json(token)
 })
 
+router.post('/verify-token', (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        res.json(decoded)
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
+})
+
 module.exports = router
